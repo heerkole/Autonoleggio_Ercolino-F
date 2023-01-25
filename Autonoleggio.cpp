8a3b8708683f21ef2f9b81e222123d0eea66dad2
heerkole <fran.erco25@outlook.it>
2023-01-25 15:22:06 +0100

    Ultime modifiche al file "Autonoleggio.cpp"

--- Autonoleggio.cpp
+++ Autonoleggio.cpp
@@ -9,11 +9,11 @@ struct InfoAuto
     string marca;
     string modello;
     string colore;
-    char lun, mar, mer, gio, ven, sab, dom;
+    char VettGiorni[6];
 };
-InfoAuto VettInfoAuto[7]
+InfoAuto VettInfoAuto[6];
 
-void CaricaVett(InfoAuto VettInfoAuto[])
+void CaricaVett()
 {
     ifstream f("auto.csv", ios::in);
     string linea;
@@ -27,14 +27,12 @@ void CaricaVett(InfoAuto VettInfoAuto[])
              >>VettInfoAuto[i].marca
              >>VettInfoAuto[i].modello
              >>VettInfoAuto[i].modello
-             >>VettInfoAuto[i].colore
-             >>VettInfoAuto[i].lun
-             >>VettInfoAuto[i].mar
-             >>VettInfoAuto[i].mer
-             >>VettInfoAuto[i].gio
-             >>VettInfoAuto[i].ven
-             >>VettInfoAuto[i].sab
-             >>VettInfoAuto[i].dom;
+             >>VettInfoAuto[i].colore;
+
+             for(int j=0; j<6; j++)
+             {
+                f>>VettInfoAuto[i].VettGiorni[i];
+             }
         }
     }
 }
@@ -49,59 +47,51 @@ string controlla_giorno(int contatore, char vett[])
 
 void controlla_disp(string Categoria, int Giorni)
 {
-    string linea, appoggio, modello;
-    int giorno, cont;
+    CaricaVett();
+
+    string linea,modello,str_intera;
+    bool contr = false;
+    int giorno, cont, q;
     string giorni_sett[7] = {"Lunedi", "Martedi", "Mercoledi", "Giovedi", "Venerdi", "Sabato", "Domenica"};
     fstream f("auto.csv", ios::in | ios::out);
 
     if(!f) cout<<" Errore nell'apertura del file!";
     else
-    {   
-        cout<<" Le macchine disponibili per la categoria selezionata sono : \n ";  
-        
+    {           
         //Ciclo di ricerca vettura per la categoria selezionata
-        
+        cout<<" Le macchine disponibili per la categoria selezionata sono : \n"; 
+
+        for(int a=0; a<6; a++)
+        {
+            if(VettInfoAuto[a].categoria == Categoria)
+                cout<<"- "<<VettInfoAuto[a].modello<<endl;
+        }                             
         
         //Scelta della vettura in base alla categoria selezionata
         cout<<"\n\n Selezionare il modello della vettura da noleggiare : ";
         cin>>modello;
 
-        if((modello!="FIAT")||(modello!="KIA")||(modello!="Mercedes")||(modello!="Lamborghini")||(modello!="Ford")||(modello!="BMW")||(modello!="Peugeot")||(modello!="Audi"))
-            cout<<" La vettura selezionata non fa parte della categoria scelta!\n";
-        else 
+        for(q=0; q<6; q++)
+        {
+            if(VettInfoAuto[q].modello==modello)
+            {
+                contr = true;
+                break;
+            }
+            else cout<<" La vettura inserita non fa parte della categoria scelta!\n";
+        }
+
+        if(contr)
         {
             //Output disponibilità modelllo vettura selezionata
             cout<<" I giorni i cui si può prenotare la vettura sono : \n ";
             
             for(int i=0; i<6; i++)
             {
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].lun == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
+                if (VettInfoAuto[q].VettGiorni[q] == 'L')
+                    cout<<giorni_sett[i]<<" : Disponibile\n";     
+                else cout<<giorni_sett[i]<<" : Non Disponibile\n";     
 
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].mar == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
-
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].mer == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
-                
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].gio == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
-
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].ven == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
-                
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].sab == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";
-
-                cout<<giorni_sett[i];
-                if(VettInfoAuto[i].dom == 'L') cout<<": Disponibile\n"; 
-                    else cout<<" : Non disponibile\n";       
             }
 
             //Output dei giorni in cui la vettura e disponibile o meno
@@ -118,9 +108,8 @@ void controlla_disp(string Categoria, int Giorni)
                 if((giorno>7)||(giorno<0))
                     cout<<" Giorno non valido!\n";
                 else
-                    vett_giorni[giorno] = 'A'; 
+                    VettInfoAuto[j].VettGiorni[j] = 'A'; 
             }
-
             cout<<" Vettura prenotata correttamente!\n";
         }  
         f.close();
@@ -132,7 +121,7 @@ void Scegli_Auto()
     string categoria;
     int giorni;
 
-    cout<<" Benvenutx all'Autonoleggio!\n"
+    cout<<"\n\n Benvenutx all'Autonoleggio!\n"
         <<" Selezionare categoria e giorni della vettura da voler affittare scegliendo tra le seguenti : \n\n"
         <<" 1. Utilitaria, 2. Lusso, 3. Sportiva, 4. Furgone\n"
         <<" >> ";
